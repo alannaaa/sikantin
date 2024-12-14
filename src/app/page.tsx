5bb60bc9ee431cc11e864d89c23c08744203d5f5
alanna <alanna@openwaygroup.com>
2024-12-15 01:13:09 +0700

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,72 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  return (
+    <div className="relative w-full min-h-screen flex flex-col bg-[#fcf5c1]">
+
+      {/* Navbar */}
+      <div className="w-full h-[95px] px-[23px] py-[17px] left-0 top-0 absolute bg-[#fffad8] justify-start items-center gap-[21px] inline-flex">
+        <img src="assets/images/starNavbar.svg" />
+        <div className="text-black text-4xl font-bold">SiKantin</div>
+      </div>
+
+      {/* Hero */}
+      <div className="grid md:grid-cols-2 mt-24">
+
+        {/* Left */}
+        <div className="w-auto grid content-center ml-10 pr-2 pt-10 md:ml-40 pb-10">
+          <div className="flex-col justify-center items-start gap-5 inline-flex xl:w-full lg:w-1/3">
+            <div className="leading-snug text-black text-3xl md:text-6xl font-bold">Pesan makan dimana saja, kapan saja, tanpa ribet</div>
+            <div className=" text-black text-xl w-5/6 font-normal">Jelajahi pilihan makanan, cek menu, dan pesan makanan dengan mudah dari seluruh kantin Universitas Indonesia.</div>
+          </div>
+          <div className="h-16 w-1/2 px-6 py-2.5 my-10 bg-[#ffd603] rounded-full justify-center items-center gap-2.5 inline-flex">
+            <div className="text-center text-black text-3xl font-medium">Pesan sekarang</div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        {/* Right */}
+        <img className="w-90 h-5/6 justify-self-end" src="assets/images/HeroRight.png" />
+      </div>
+
+      {/* 3 stars */}
+      <div className="bg-[#FFFCE4] px-48 py-24 gap-[120px] flex-col justify-center items-center inline-flex">
+        <div className="grid grid-cols-3 gap-48">
+          <div className="flex flex-col gap-4 justify-center">
+            <img className="w-[202px] h-auto mx-auto" src="assets/images/cari.png" />
+            <div className="text-center text-black text-3xl 2xl:text-4xl">Cari menu</div>
+          </div>
+          <div className="flex flex-col gap-4  justify-center">
+            <img className="w-[202px] h-auto mx-auto" src="assets/images/pesan.png" />
+            <div className="text-center text-black text-3xl 2xl:text-4xl">Buat pesanan</div>
+          </div>
+          <div className=" flex flex-col gap-4  justify-center">
+            <img className="w-[230px] h-auto mx-auto" src="assets/images/ambil.png" />
+            <div className="text-center text-black text-3xl 2xl:text-4xl">Ambil saat siap</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-center gap-[22px] inline-flex">
+          <div className="text-black text-3xl 2xl:text-4xl">Semua di aplikasi <span className="font-semibold">SiKantin</span></div>
+          <div className="justify-start items-center gap-[25px] inline-flex">
+            <img className="w-[233px] h-[79px]" src="assets/images/appstore.png" />
+            <img className="w-[245.89px] h-[75px]" src="assets/images/gplay.png" />
+          </div>
+        </div>
+      </div>
+
+      {/* Menu populer */}
+      <div className="px-[74px] py-[115px] flex-col justify-start items-center gap-[95px] inline-flex">
+        <div className="text-center text-black text-[64px] font-semibold font-['Plus Jakarta Sans']">Menu Populer</div>
+        <div className="grid grid-cols-3 gap-9">
+          <img className="" src="assets/images/menu1.png" />
+          <img className="" src="assets/images/menu 2.png" />
+          <img className="" src="assets/images/menu3.png" />
+        </div>
+        <div className="h-16 px-8 py-2.5  bg-[#ffd603] rounded-full justify-center items-center gap-2.5 inline-flex">
+            <div className="text-center text-black text-3xl font-medium">Lihat menu lainnya</div>
+          </div>
+      </div>
     </div>
   );
 }
